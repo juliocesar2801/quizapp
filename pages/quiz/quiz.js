1bf389c5a35dc13ffae4529058b43221796a2f09
juliocesar2801 julio <julio.cocco@eaportal.org>
2024-06-06 01:05:45 +0000

    adicinada a funçao de guradar resposta no id

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -6,7 +6,9 @@
 
     let quiz = {}
     let pontos = 0
-    let pergunta = 2
+    let pergunta = 1
+    let resposta = ""
+    let idInputResposta = ""
 
     botaoTema.addEventListener("click", () => {
     trocarTema(body,botaoTema)
@@ -53,14 +55,14 @@
     <section class="alternativas">
         <form action="">
            <label for="alternativa_a" >
-            <input type="radio" id="alternativa_a" name="alternativa">
+           <input type="radio" id="alternativa_a" name="alternativa" value="${alterarSinais(quiz.questions[pergunta-1].options[0])}">
             <div>
                 <span>A</span>
                 ${alterarSinais(quiz.questions[pergunta-1].options[0])}
             </div>
            </label> 
            <label for="alternativa_b" >
-            <input type="radio" id="alternativa_b" name="alternativa">
+           <input type="radio" id="alternativa_b" name="alternativa" value="${alterarSinais(quiz.questions[pergunta-1].options[2])}">
             <div>
                 <span>B</span>
                 ${alterarSinais(quiz.questions[pergunta-1].options[1])}
@@ -68,14 +70,14 @@
            </label> 
            
            <label for="alternativa_c">
-            <input type="radio" id="alternativa_c" name="alternativa">
+           <input type="radio" id="alternativa_c" name="alternativa" value="${alterarSinais(quiz.questions[pergunta-1].options[2])}">
             <div>
                 <span>C</span>
                 ${alterarSinais(quiz.questions[pergunta-1].options[2])}
             </div>
            </label> 
            <label for="alternativa_d">
-            <input type="radio" id="alternativa_d" name="alternativa">
+           <input type="radio" id="alternativa_d" name="alternativa" value="${alterarSinais(quiz.questions[pergunta-1].options[2])}">
             <div>
                 <span>D</span>
                 ${alterarSinais(quiz.questions[pergunta-1].options[3])}
@@ -89,10 +91,22 @@
     function alterarSinais(texto){
         return texto.replace(/</g, "&lt;").replace(/>/g , "&gt;")
     }
+
+    function guardarResposta(evento){
+        resposta = evento.target.value
+        idInputResposta = evento.target.id
+    
+    }
+
     async function iniciar() {
         alterarAssunto()
         await buscarPerguntas()
         montarPergunta()
     }
+
+    const inputsResposta =  document.querySelectorAll(".alternativas input")
+    inputsResposta.forEach(input => {
+        input.addEventListener("click",guardarResposta)
+    })
     
     iniciar()
